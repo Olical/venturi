849d88cb3d3efdeac0d4236dc608d6bd09a3ce96
Oliver Caldwell <olliec87@gmail.com>
2014-03-08 15:48:18 +0000

    Added tests for links.

--- test/links.js
+++ test/links.js
@@ -11,5 +11,34 @@ describe('Links', function () {
 	});
 
 	describe('given one instance that links to another but is in a different part of the inheritance tree', function () {
+		var providingInjector;
+		var consumingInjector;
+
+		beforeEach(function () {
+			providingInjector = injector.module();
+			consumingInjector = injector.module(providingInjector);
+		});
+
+		describe('where the provider contains a value', function () {
+			beforeEach(function () {
+				providingInjector.set('foo', function () {
+					return {bar:true};
+				});
+			});
+
+			it('should allow the consumer to access the value in the provider', function () {
+				var result = consumingInjector.get('foo');
+				result.foo.should.have.property('bar', true);
+			});
+
+			it('should share the instances between the two injectors', function () {
+				var providerResult = providingInjector.get('foo');
+				var consumerResult = consumingInjector.get('foo');
+				providerResult.foo.one = 1;
+				consumerResult.foo.two = 2;
+				providerResult.foo.two.should.equal(2);
+				consumerResult.foo.one.should.equal(1);
+			});
+		});
 	});
 });
