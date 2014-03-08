98dd29bceb4c6c77444dc0dccb5ba1cf184e3978
Oliver Caldwell <olliec87@gmail.com>
2014-03-08 15:18:23 +0000

    Blank linking test file.

---
+++ test/links.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var Venturi = require('..');
+require('should');
+
+describe('Links', function () {
+	var injector;
+
+	beforeEach(function () {
+		injector = new Venturi();
+	});
+
+	describe('given one instance that links to another but is in a different part of the inheritance tree', function () {
+	});
+});
