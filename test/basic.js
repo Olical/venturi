f1454552426c0e3753a53c3caabed5a2c92f0cec
Oliver Caldwell <olliec87@gmail.com>
2014-03-08 11:39:43 +0000

    Added basic first test.
    Just checking for a set/get pair really.

--- test/basic.js
+++ test/basic.js
@@ -1,3 +1,27 @@
 'use strict';
 
-var should = require('should')
+var Venturi = require('..');
+var sinon = require('sinon');
+require('should');
+
+describe('Basic', function () {
+	var injector;
+
+	beforeEach(function () {
+		injector = new Venturi();
+	});
+
+	describe('given an injector with an injected constructor', function () {
+		var constructorSpy;
+
+		beforeEach(function () {
+			constructorSpy = sinon.stub().returns('bar');
+			injector.set('foo', constructorSpy);
+		});
+
+		it('should return an object with the injected value inside', function () {
+			var result = injector.get('foo');
+			result.should.have.property('foo', 'bar');
+		});
+	});
+});
