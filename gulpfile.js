27725b69d3054f87bda23a407f3c2ae8bddee5c4
Oliver Caldwell <olliec87@gmail.com>
2014-03-08 10:53:28 +0000

    Improved test / linting setup.

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,21 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var mocha = require('gulp-mocha');
+var stylish = require('jshint-stylish');
+
+var paths = {
+	source: 'index.js',
+	tests: 'test/**/*.js'
+};
 
 gulp.task('lint', function () {
-	return gulp.src('index.js')
-		.pipe(jshint());
+	return gulp.src([paths.source, paths.tests])
+		.pipe(jshint())
+		.pipe(jshint.reporter(stylish));
 });
 
 gulp.task('test', function () {
-	return gulp.src('test.js')
+	return gulp.src(paths.tests)
 		.pipe(mocha());
 });
 
