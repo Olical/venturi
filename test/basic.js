c44ca703f71d290aae074a2b00c45181175b371a
Oliver Caldwell <olliec87@gmail.com>
2014-03-08 11:51:35 +0000

    Added a multiple dependencies test.

--- test/basic.js
+++ test/basic.js
@@ -2,6 +2,7 @@
 
 var Venturi = require('..');
 var sinon = require('sinon');
+var _ = require('lodash');
 require('should');
 
 describe('Basic', function () {
@@ -22,6 +23,38 @@ describe('Basic', function () {
 		it('should return an object with the injected value inside', function () {
 			var result = injector.get('foo');
 			result.should.have.property('foo', 'bar');
+		});
+
+		it('should only execute the constructor once', function () {
+			_.times(5, function () {
+				injector.get('foo');
+			});
+			constructorSpy.callCount.should.equal(1);
+		});
+	});
+
+	describe('given an injector with mutiple injected constructors', function () {
+		var stubs;
+
+		beforeEach(function () {
+			stubs = {
+				one: sinon.stub().returns(1),
+				two: sinon.stub().returns(2),
+				three: sinon.stub().returns(3)
+			};
+
+			_.forIn(stubs, function (value, key) {
+				injector.set(key, value);
+			});
+		});
+
+		it('should all requested dependencies under the correct names', function () {
+			var result = injector.get('one', 'three');
+			result.should.not.have.property('two');
+			result.should.have.properties({
+				one: 1,
+				three: 3
+			});
 		});
 	});
 });
